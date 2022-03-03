338b3db0384108512589268ca53883598ea9cf7e
Peter SF <petersackettferguson@gmail.com>
2022-03-03 16:34:39 -0500

    Improve warping and add white balance/color correction

--- imgproc.py
+++ imgproc.py
@@ -4,6 +4,7 @@ import vis
 
 import math
 import cv2
+from cv2 import xphoto
 import numpy as np
 from glob import glob
 
@@ -11,6 +12,8 @@ DEBUG = True
 # makes output match image, but internal processing is mirrored
 FLIP = True
 RES = 1000
+
+WARP_METHOD = "ROTATE_TEE"
 
 BLU0 = np.array([85,30,30])
 BLU1 = np.array([105,255,255])
@@ -21,17 +24,65 @@ RED1_1 = np.array([20,255,190])
 RED2_0 = np.array([170,90,100])
 RED2_1 = np.array([180,255,190])
 
-#image must be in HSV format
-def find_centers(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def find_tee(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # SHEET #
+    # FIND TEE
+    mask_blue = cv2.inRange(hsv, BLU0, BLU1)
+    res_blue = cv2.bitwise_and(hsv,hsv, mask=mask_blue)
+    gray_blue = cv2.cvtColor(res_blue, cv2.COLOR_BGR2GRAY)
+    _,thresh_blue = cv2.threshold(gray_blue,10,255,cv2.THRESH_BINARY)
+    bdbg = cv2.cvtColor(thresh_blue, cv2.COLOR_GRAY2BGR)
+
+    bcircs = cv2.HoughCircles(thresh_blue,cv2.HOUGH_GRADIENT,2.05,RES/4)[0]
+    bcircs = [list([c for c in h]) for h in bcircs]
+
+    r12 = 0.0
+    tee = None
+    for c in bcircs:
+        x, y, radius = c
+        center = (x, y)
+        if x > RES/3 and x < 2*RES/3 and radius < RES/2 and y > RES/2:
+            if radius > r12:
+                r12 = radius
+                tee = center
+
+        if DEBUG:
+            c = [int(f) for f in center]
+            r = int(radius)
+            cv2.circle(bdbg, c, r, (240, 240, 0), 2)
+
+    if tee is None:
+        return None
+
+    if DEBUG:
+        c = [int(f) for f in tee]
+        r = int(r12)
+        cv2.circle(bdbg, c, r, (255, 0, 0), 5)
+
+    return bdbg, c, r
+
+
+# WARP IMAGE SO SHEET SIDES ARE VERTICAL
+# 1: filter image to highlight sheet vs non-sheet
+# 2: detect lines indicating sides of sheet. Average left and right groups.
+# 3: find the bisecting angle of these lines to find the required rotation
+# 4: calculate warp matrix to rotate + fix perspective
+def warp(img, tee=None, method=None):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _,thresh_gray = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
     blur_gray = cv2.blur(thresh_gray, (3,3))
     canny = cv2.Canny(blur_gray, 100, 200)
     canny_blur = cv2.blur(canny, (5, 5))
 
+    warped = img.copy()
     ldbg = cv2.cvtColor(thresh_gray, cv2.COLOR_GRAY2BGR)
+    lb = 0
+    rb = RES
+
+    if method is None:
+        return warped,ldbg,lb,rb
 
     lines = cv2.HoughLinesP(canny_blur,1,0.01,10,minLineLength=RES/2)
     lines = [l[0] for l in lines]
@@ -45,9 +96,6 @@ def find_centers(img):
     llines = list()
     rlines = list()
 
-    warped = img.copy()
-    lb = 0
-    rb = RES
     for line in sidelines:
         x1,y1,x2,y2=line
         if (x1+x2)/2 < RES /2:
@@ -57,25 +105,89 @@ def find_centers(img):
     if len(llines) > 0 and len(rlines) > 0:
         lbound = np.mean(llines,axis=0)
         rbound = np.mean(rlines,axis=0)
-        l1 = [lbound[0], lbound[1]]
-        l2 = [lbound[2], lbound[3]]
-        r1 = [rbound[0], rbound[1]]
-        r2 = [rbound[2], rbound[3]]
-        if l1[1] > l2[1]:
-            l1, l2 = l2, l1
-        if r1[1] > r2[1]:
-            r1, r2 = r2, r1
-        l2d = [l1[0], l2[1]]
-        r2d = [r1[0], r2[1]]
 
-        ini = np.float32([l1, l2, r1, r2])
-        fin = np.float32([l1, l2d, r1, r2d])
+        
+        lm = (lbound[2]-lbound[0])/(lbound[3]-lbound[1])
+        rm = (rbound[2]-rbound[0])/(rbound[3]-rbound[1])
+        la = math.atan(lm)
+        ra = math.atan(rm)
+        theta = la + ra
+        def lbfy(y):
+            b = lbound[0] - (lm*lbound[1])
+            return (lm*y)+b
+        def rbfy(y):
+            b = rbound[0] - (rm*rbound[1])
+            return (rm*y)+b
+        def rotate(pt, center):
+            x, y = pt
+            cx, cy = center
+            sx, sy = x - cx, y - cy
+            rx = sx*math.cos(theta) - sy*math.sin(theta) + cx
+            ry = sx*math.sin(theta) + sy*math.cos(theta) + cy
+            return [rx, ry]
+
+        ini = None
+        fin = None
+        if method == "ROTATE_BASE":
+            l0 = [lbfy(0), 0]
+            r0 = [rbfy(0), 0]
+            c = [(l0[0] + r0[0])/2, 0]
+            l1 = [lbfy(RES), RES]
+            r1 = [rbfy(RES), RES]
+            l0d = rotate(l0, c)
+            r0d = rotate(r0, c)
+            l1d = rotate(l1, c)
+            r1d = rotate(r1, c)
+
+            ini = np.float32([l0, l1, r0, r1])
+            fin = np.float32([l0d, l1d, r0d, r1d])
+        elif method == "ROTATE_TEE":
+            if tee is None:
+                raise Exception("tee required if using method ROTATE_TEE")
+            l0 = [lbfy(tee[1]), tee[1]]
+            r0 = [rbfy(tee[1]), tee[1]]
+            l1 = [lbfy(RES), RES]
+            r1 = [rbfy(RES), RES]
+            l0d = rotate(l0, tee)
+            r0d = rotate(r0, tee)
+            l1d = rotate(l1, tee)
+            r1d = rotate(r1, tee)
+
+            ini = np.float32([l0, l1, r0, r1])
+            fin = np.float32([l0d, l1d, r0d, r1d])
+        elif method == "BASE":
+            l0 = [lbfy(0), 0]
+            r0 = [rbfy(0), 0]
+            c = [(l0[0] + r0[0])/2, 0]
+            l1 = [lbfy(RES), RES]
+            r1 = [rbfy(RES), RES]
+            l0d = rotate(l0, c)
+            r0d = rotate(r0, c)
+            l1d = [l0d[0], RES]
+            r1d = [r0d[0], RES]
+
+            ini = np.float32([l0, l1, r0, r1])
+            fin = np.float32([l0d, l1d, r0d, r1d])
+        elif method == "TEE":
+            if tee is None:
+                raise Exception("tee required if using method TEE")
+            l0 = [lbfy(tee[1]), tee[1]]
+            r0 = [rbfy(tee[1]), tee[1]]
+            l1 = [lbfy(RES), RES]
+            r1 = [rbfy(RES), RES]
+            l0d = rotate(l0, tee)
+            r0d = rotate(r0, tee)
+            l1d = [l0d[0], RES]
+            r1d = [r0d[0], RES]
+
+            ini = np.float32([l0, l1, r0, r1])
+            fin = np.float32([l0d, l1d, r0d, r1d])
 
         warp_matrix = cv2.getPerspectiveTransform(ini, fin)
         warped = cv2.warpPerspective(img,warp_matrix,(RES,RES))
 
-        lb = int(l1[0])
-        rb = int(r1[0])
+        lb = int(l0d[0])
+        rb = int(r0d[0])
 
         if DEBUG:
             lx1,ly1,lx2,ly2=[int(v) for v in lbound]
@@ -89,46 +201,52 @@ def find_centers(img):
         cv2.line(ldbg,(lb,0),(lb,RES),(0,255,0))
         cv2.line(ldbg,(rb,0),(rb,RES),(0,255,0))
 
-    hsv = cv2.cvtColor(warped, cv2.COLOR_BGR2HSV)
+    return warped, ldbg, lb, rb
+
+# BROKEN / INCOMPLETE
+def color_correct(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _,thresh_gray = cv2.threshold(gray,180,255,cv2.THRESH_BINARY)
+    sheet = cv2.bitwise_and(img,img, mask=thresh_gray)
+    flat = sheet.reshape((RES*RES, 3))
+    filtered = []
+    for b,g,r in flat:
+        if b != 0 and g != 0 and r != 0:
+            filtered.append([b,g,r])
+
+
+    mb,mg,mr = np.mean(filtered, axis=(0))
+    cfb,cfg,cfr = 250.0/mb, 250.0/mg, 250.0/mr
+    corrected = img * [cfb, cfg, cfr]
+
+    return corrected
+
+
+def find_centers(img):
+    # FIND TEE
+    bdbg, tee, r12 = find_tee(img)
+
+    # WARP SHEET
+    warped,ldbg,lb,rb = warp(img, tee=tee, method=WARP_METHOD)
     gdbg = cv2.cvtColor(cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
 
-    # BLUE / SCALE #
-    mask_blue = cv2.inRange(hsv, BLU0, BLU1)
-    res_blue = cv2.bitwise_and(hsv,hsv, mask=mask_blue)
-    gray_blue = cv2.cvtColor(res_blue, cv2.COLOR_BGR2GRAY)
-    _,thresh_blue = cv2.threshold(gray_blue,10,255,cv2.THRESH_BINARY)
-    bdbg = cv2.cvtColor(thresh_blue, cv2.COLOR_GRAY2BGR)
-
-    bcircs = cv2.HoughCircles(thresh_blue,cv2.HOUGH_GRADIENT,2.05,RES/4)[0]
-    bcircs = [list([c for c in h]) for h in bcircs]
-
-    r12 = 0.0
-    tee = None
-    w = float(hsv.shape[1])
-    for c in bcircs:
-        x, y, radius = c
-        center = (x, y)
-        if x > w/3 and x < 2*w/3 and x-radius > lb and x+radius < rb:
-            if radius > r12:
-                r12 = radius
-                tee = center
-
-        if DEBUG:
-            c = [int(f) for f in center]
-            r = int(radius)
-            cv2.circle(bdbg, c, r, (240, 240, 0), 2)
-
+    # ADJUST TEE
+    #bdbg, tee, r12 = find_tee(warped)
     scale = constants.TWELVE / r12
-    if tee is None:
-        return None
+
+    # COLOR CORRECT
+    #cc = color_correct(warped)
+    wb = cv2.xphoto.createSimpleWB()
+    cc = wb.balanceWhite(warped)
 
     if DEBUG:
         c = [int(f) for f in tee]
         r = int(r12)
-        cv2.circle(bdbg, c, r, (255, 0, 0), 5)
         cv2.circle(gdbg, c, r, (255, 0, 0), 5)
 
+
     # ROCKS #
+    hsv = cv2.cvtColor(cc, cv2.COLOR_BGR2HSV)
     mask_yellow = cv2.inRange(hsv, YEL0, YEL1)
     mask_red1 = cv2.inRange(hsv, RED1_0, RED1_1)
     mask_red2 = cv2.inRange(hsv, RED2_0, RED2_1)
